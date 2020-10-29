33653f10adb920800d1aaab9791f3fbab6b76ab2
burnca02 <62625876+burnca02@users.noreply.github.com>
2020-10-28 21:19:33 -0400

    Signup/Login(10/28)
    New Signup/Login

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://yoda:maytheforce@cluster0.fvmkx.mongodb.net/test?retryWrites=true&w=majority'
+}
--- models/User.js
+++ models/User.js
@@ -1,21 +1,24 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
 
-const userSchema = new Schema({
-    name: {
-        type: String
-    },
-    email: {
-        type: String
-    },
-    phone: {
-        type: String
+ const UserSchema = new mongoose.Schema({
+     name: {
+         type: String,
+         required: true
+     },
+     email: {
+        type: String,
+        required: true
     },
     password: {
-        type: String
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
-}, {timestamps: true})
+ })
 
-const User = mongoose.model('User', userSchema)
-module.exports = User
+ const User = mongoose.model('User', UserSchema);
 
+ module.exports = User;
